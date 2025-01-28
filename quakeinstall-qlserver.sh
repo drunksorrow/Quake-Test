f3ff8e9f0688cbfb818dd1b9eeb5ddce5bd76029
drunksorrow <69868026+drunksorrow@users.noreply.github.com>
2025-01-28 21:24:53 +0200

    Update quakeinstall-qlserver.sh

--- quakeinstall-qlserver.sh
+++ quakeinstall-qlserver.sh
@@ -1,29 +1,19 @@
-#!/bin/bash
+#! /bin/bash
 # quakeinstall-qlserver.sh - quake live dedicated server installation for qlserver user.
 
-if [ "$(whoami)" != "qlserver" ]; then
-  echo "Please run under user 'qlserver'."
-  exit 1
+if [ "$(whoami)" -ne "qlserver" ]
+  then echo "Please run under user 'qlserver'."
+  exit
 fi
-
 clear
 echo "Installing SteamCMD..."
-mkdir -p ~/steamcmd
-cd ~/steamcmd
-wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-tar -xvzf steamcmd_linux.tar.gz
-rm steamcmd_linux.tar.gz
-
+mkdir ~/steamcmd; cd ~/steamcmd; wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz; tar -xvzf steamcmd_linux.tar.gz; rm steamcmd_linux.tar.gz
 clear
 echo "Installing Quake Live Dedicated Server..."
 ./steamcmd.sh +login anonymous +force_install_dir /home/qlserver/steamcmd/steamapps/common/qlds/ +app_update 349090 +quit
-
 clear
 #echo "Cronning 'QuakeUpdate.sh'..."
-#echo "0 8 * * * /home/qlserver/quakeupdate.sh" > cron
-#crontab cron
-#rm cron
-
-clear
+#echo "0 8 * * * /home/qlserver/quakeupdate.sh" > cron; crontab cron; rm cron
+#clear
 echo "Done."
-exit 0
+exit
