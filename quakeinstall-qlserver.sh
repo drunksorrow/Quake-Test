ae7d536f1e3a680c06a603707c8d1041dd128ddc
drunksorrow <69868026+drunksorrow@users.noreply.github.com>
2025-01-28 20:14:53 +0200

    Update quakeinstall-qlserver.sh

--- quakeinstall-qlserver.sh
+++ quakeinstall-qlserver.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
-# quakeinstall-root.sh - Install Quake Live server on Ubuntu 24.04 for root user
+# quakeinstall-qlserver.sh - Install Quake Live server for qlserver user
 
-echo "Updating 'apt-get'..."
+if [ "$(whoami)" != "qlserver" ]; then
+  echo "Please run this script as the 'qlserver' user."
+  exit 1
+fi
+
+echo "Installing required packages for qlserver..."
 sudo apt-get update -y
+sudo apt-get -y install python3 python3-dev redis-server build-essential git
 
-echo "Installing required packages..."
-sudo apt-get -y install libc6:i386 zlib1g:i386 build-essential git
-
-echo "Checking and installing 32-bit libraries..."
-if ! dpkg --print-architecture | grep -q "i386"; then
-  sudo dpkg --add-architecture i386
-  sudo apt-get update
-fi
-
-echo "Installing required dependencies..."
-sudo apt-get -y install libc6:i386 zlib1g:i386
-
-echo "Installing ZeroMQ library..."
-if ! wget http://download.zeromq.org/zeromq-4.1.4.tar.gz; then
-  echo "Failed to download ZeroMQ from the official server, trying manual download..."
-  wget https://github.com/zeromq/libzmq/releases/download/v4.1.4/zeromq-4.1.4.tar.gz || {
-    echo "ZeroMQ download failed. Please download manually from https://github.com/zeromq/libzmq/releases and place it in your current directory."
-    exit 1
-  }
-fi
-
-# Dezarhivează și instalează ZeroMQ
-tar -xvzf zeromq-4.1.4.tar.gz
-cd zeromq-4.1.4 || exit
-./configure
+echo "Cloning and compiling Quake Live server..."
+cd ~/steamcmd/steamapps/common/qlds || exit
+git clone https://github.com/MinoMino/minqlx.git
+cd minqlx || exit
 make
-sudo make install
+cp -r bin/* ..
 
-echo "Installation of dependencies completed successfully."
+echo "Installing Minqlx plugins and dependencies..."
+cd ~/steamcmd/steamapps/common/qlds || exit
+git clone https://github.com/MinoMino/minqlx-plugins.git
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+rm get-pip.py
+export PIP_BREAK_SYSTEM_PACKAGES=1  # For Debian 12+ users
+python3 -m pip install -r minqlx-plugins/requirements.txt
+
+echo "Minqlx setup is complete. Please configure it with your SteamID64."
